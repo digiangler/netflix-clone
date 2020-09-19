26504ae8fd1749b9e4afa13bdc278d559c25fc16
Digi-Angler <digiangler777@gmail.com>
2020-09-19 21:39:26 +0900

    history folder delete

--- .history/src/Requests_20200917161259.js
+++
@@ -1,1 +0,0 @@
-const API_KEY = "8712b0610496fec8b39847307d977e5f";
--- .history/src/youtube_20200919163845.js
+++
@@ -1,7 +0,0 @@
-import axios from "axios";
-
-const YoutubeKey = axios.create({
-  baseURL: "https://www.googleapis.com/youtube/v3",
-});
-
-export default YoutubeKey;
